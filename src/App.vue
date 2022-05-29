cc3165524e9d457c02085dd6367b3325870cd693
Sattor <60736981+sattor1@users.noreply.github.com>
2022-05-29 21:50:40 +0300

    add

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view />
+  </div>
+</template>
+
+<script>
+import Main from "./main/index.vue";
+
+export default {
+  name: "App",
+
+  components: {
+    Main
+  }
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  height: 100%;
+  max-width: 1198px;
+  margin: 0 auto;
+}
+</style>
